759c69b628cafcdc5b20958e8c3be2d4bf14c0e7
karlpn <91100704+karlpn@users.noreply.github.com>
2024-04-18 12:47:08 +0200

    jest test units fixed

--- src/js/storage/load.test.js
+++ src/js/storage/load.test.js
@@ -1,11 +1,14 @@
 import { load } from './load';
+import { save } from './save';
 import localStorageMock from './localStorage.mock';
 
 global.localStorage = localStorageMock;
 
 describe('load function', () => {
   it('is expected to load item from localstorage', () => {
-    load('accesstoken');
-    expect(localStorage.token).toEqual(undefined);
+    const token = 'someAccessToken';
+    save('token', token);
+    load('token');
+    expect(localStorage.token).toEqual(JSON.stringify(token));
   });
 });
